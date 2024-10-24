6d8b4899b95e5b5f21a10708c9a9263d0c47c192
usmanramzan342 <usmantt.ramzan@gmail.com>
2024-10-24 19:24:17 +0500

    TODO APP

--- app.js
+++ app.js
@@ -1,54 +1,125 @@
 
 
-const form = document.querySelector("#todo-form");
-const list = document.querySelector("#todo-list");
-const saveList = document.querySelector("#saveList");
-const todolist = [
-    "NAMAZ",
-    "practice",
-    "github profile",
-    "leetcode",
+let form = document.getElementById("form");
+let ulMain = document.getElementById("main-ul");
 
-];
-    
-// const savelist = [
+let savedItems = localStorage.getItem("li");
+if (savedItems) {
 
-// ];
-// localStorage.setItem("saveList", JSON.stringify(saveList));
-// const save = JSON.parse(localStorage.getItem("saveList"));
+    ulMain.innerHTML = JSON.parse(savedItems);
+}
 
-localStorage.setItem("mulist", JSON.stringify(todolist));
-
-const mulist = JSON.parse(localStorage.getItem("mulist"));
-
-console.log("mulist" , mulist);
-
-
-
-// for (let i = 0; i < todolist.length; i++) {
-//     const value = todolist[i]
-//     list.innerHTML += `<li>${todolist[i]}</li>`    
-     
-// };
-
-
-
-
-// const <li> </li> = localStorage.getItem("todomylist");
-
-todolist.map(function(value , index) {
-    console.log( index,"usman" , value);
-     list.innerHTML += `<li>${value }</li>`
-})
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
+ 
 
-    const todo = event.target.children[0].value;
+  let inputValue = event.target[0].value;
 
-    list.innerHTML += `<li>${todo} </li>`;
+  let li = document.createElement("li");
+  li.textContent = inputValue;
+  li.className = "todo-li";
+
+  ulMain.appendChild(li);
   
 
-    event.target.reset();
-    
+  localStorage.setItem("li", JSON.stringify(ulMain.innerHTML));
+
+  event.target[0].value = "";
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector("#todo-form");
+// const list = document.querySelector("#todo-list");
+// const saveList = document.querySelector("#saveList");
+// const todolist = [
+//     "NAMAZ",
+//     "practice",
+//     "github profile",
+//     "leetcode",
+
+// ];
+
+// // const savelist = [
+
+// // ];
+// // localStorage.setItem("saveList", JSON.stringify(saveList));
+// // const save = JSON.parse(localStorage.getItem("saveList"));
+
+// localStorage.setItem("mulist", JSON.stringify(todolist));
+
+// const mulist = JSON.parse(localStorage.getItem("mulist"));
+
+// console.log("mulist" , mulist);
+
+
+
+// // for (let i = 0; i < todolist.length; i++) {
+// //     const value = todolist[i]
+// //     list.innerHTML += `<li>${todolist[i]}</li>`    
+
+// // };
+
+
+
+
+// // const <li> </li> = localStorage.getItem("todomylist");
+
+// todolist.map(function(value , index) {
+//     console.log( index,"usman" , value);
+//      list.innerHTML += `<li>${value }</li>`
+// })
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault()
+
+//     const todo = event.target.children[0].value;
+
+//     list.innerHTML += `<li>${todo} </li>`;
+
+
+//     event.target.reset();
+
+// });
+
+// todo_array.map((value)=>{
+//     list.innerHTML += `<li> ${value} </li>`
+// });
+
 
